dfc80c3e2932d73f71eb72a446e2f350851e7042
changjiashuai <changjiashuai@gmail.com>
2014-06-02 10:59:45 +0800

    初始化项目

---
+++ iPhoneAPIDemo/iPhoneAPIDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iPhoneAPIDemo
+//
+//  Created by CJS on 14-6-2.
+//  Copyright (c) 2014年 常家帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPhoneAPIDemo/iPhoneAPIDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iPhoneAPIDemo
+//
+//  Created by CJS on 14-6-2.
+//  Copyright (c) 2014年 常家帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
