28df5ad97b286c84c6de311fe98a4f860498a8ac
changjiashuai <changjiashuai@gmail.com>
2014-06-03 15:46:04 +0800

    完成UIButton、UIDatePicker

--- iPhoneAPIDemo/iPhoneAPIDemo/DetailsViewController.m
+++ iPhoneAPIDemo/iPhoneAPIDemo/DetailsViewController.m
@@ -30,6 +30,15 @@
     UIView *view = [[UIView alloc] initWithFrame:applicationFrame];
     view.backgroundColor = [UIColor whiteColor];
     self.view = view;
+    
+    UIScrollView *scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 64, 320, applicationFrame.size.height - 64)];
+    NSLog(@"%f",applicationFrame.size.height - 64);
+    scrollView.contentSize = CGSizeMake(320, 640);
+//    scrollView.delegate = self;
+//    scrollView.pagingEnabled = YES;
+//    scrollView.showsHorizontalScrollIndicator = NO;
+//    scrollView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:scrollView];
 }
 
 #pragma mark - Action Sheet
@@ -242,16 +251,97 @@
 {
     if (self = [super init]) {
         //
-        self.navigationItem.title = @"";
+        self.navigationItem.title = @"Button";
+        [self configureSystemTextButton];
+        [self configureSystemContactAddButton];
+        [self configureSystemDetailDisclosureButton];
+        [self configureImageButton];
+        [self configureAttributedTextSystemButton];
     }
     return self;
 }
+
+-(void)configureSystemTextButton
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(0, 80, 320, 40);
+    [button setTitle:@"Button" forState:UIControlStateNormal];
+    button.layer.borderWidth = 0.5;
+    [self.view addSubview:button];
+}
+
+-(void)configureSystemContactAddButton
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeContactAdd];
+    button.frame = CGRectMake(0, 140, 320, 40);
+    button.backgroundColor = [UIColor clearColor];
+    button.layer.borderWidth = 0.5;
+    [self.view addSubview:button];
+}
+
+-(void)configureSystemDetailDisclosureButton
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    button.frame = CGRectMake(0, 200, 320, 40);
+    button.backgroundColor = [UIColor clearColor];
+    button.layer.borderWidth = 0.5;
+    [self.view addSubview:button];
+}
+
+-(void)configureImageButton
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(0, 260, 320, 40);
+    [button setImage:[UIImage imageNamed:@"x_icon"] forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor clearColor];
+    button.layer.borderWidth = 0.5;
+    // 设置圆角
+    button.layer.cornerRadius = 0.0;
+    // 设置颜色空间为rgb，用于生成ColorRef
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//    // 新建一个红色的ColorRef，用于设置边框（四个数字分别是 r, g, b, alpha）
+//    CGColorRef borderColorRef = CGColorCreate(colorSpace,(CGFloat[]){ 0.3, 0.3, 0, 1 });
+//    // 设置边框颜色
+//    button.layer.borderColor = borderColorRef;
+    [self.view addSubview:button];
+}
+
+-(void)configureAttributedTextSystemButton
+{
+    
+    NSDictionary *titleAttributes = @{NSForegroundColorAttributeName: [UIColor redColor], NSStrikethroughStyleAttributeName: @(NSUnderlineStyleSingle)};
+    NSAttributedString *attributedTitle = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Button", nil) attributes:titleAttributes];
+    
+    
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = CGRectMake(0, 320, 320, 40);
+    button.backgroundColor = [UIColor clearColor];
+    button.layer.borderWidth = 0.5;
+    
+    
+    [button setAttributedTitle:attributedTitle forState:UIControlStateNormal];
+    
+    NSDictionary *highlightedTitleAttributes = @{NSForegroundColorAttributeName : [UIColor orangeColor], NSStrikethroughStyleAttributeName: @(NSUnderlineStyleThick)};
+    NSAttributedString *highlightedAttributedTitle = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Button", nil) attributes:highlightedTitleAttributes];
+    [button setAttributedTitle:highlightedAttributedTitle forState:UIControlStateHighlighted];
+    
+    [self.view addSubview:button];
+}
+
+
+#pragma mark - Date Picker
 
 -(instancetype)initWithDatePicker
 {
     if (self = [super init]) {
         //
-        self.navigationItem.title = @"";
+        self.navigationItem.title = @"Date Picker";
+        
+        UIDatePicker *datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 100, 320, 216)];
+        
+        [self.view addSubview:datePicker];
     }
     return self;
 }
